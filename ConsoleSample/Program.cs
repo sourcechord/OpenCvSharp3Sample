48a62d6193f5ee49d8d5838cbb8da6460b4c0c19
minami <sourcechord@gmail.com>
2016-08-11 12:06:26 +0900

    Initial Commit

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var src = new Mat(@"Images/1.jpg");
+            var dst = new Mat();
+
+            Cv2.CvtColor(src, dst, ColorConversionCodes.BGRA2GRAY);
+
+            using (new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }
+        }
+    }
+}
---
+++ WpfSample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var src = new Mat(@"Images/1.jpg");
+            var dst = new Mat();
+            Cv2.CvtColor(src, dst, ColorConversionCodes.BGRA2GRAY);
+            imgResult.Source = dst.ToWriteableBitmap();
+        }
+    }
+}
